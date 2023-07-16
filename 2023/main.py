7d6f9fa68345561f6294d58779d6d42a9ce90844
SeanMabli <smdrone1@gmail.com>
2023-07-16 12:05:15 -0400

    non recursive closestContainer, cloesestBoat functions, remove unessasary code, add timing system, small tweaks to movement

--- 2023/main.py
+++ 2023/main.py
@@ -4,7 +4,7 @@ from pybricks.hubs import EV3Brick
 from pybricks.ev3devices import Motor, ColorSensor, GyroSensor
 from pybricks.parameters import Port, Color, Button, Direction
 from pybricks.robotics import DriveBase
-import time, _thread, math, json, random
+import time, _thread, random
 
 ArmMotor = Motor(Port.A)
 RightMotor = Motor(Port.B)
@@ -18,6 +18,20 @@ RightColor = ColorSensor(Port.S3)
 
 robot = DriveBase(LeftMotor, RightMotor, wheel_diameter=94.2, axle_track=157)
 robot.settings(straight_speed=700)
+
+times = {}
+
+def timefunc(func):
+  def inner(*args, **kwargs):
+    timeA = time.time()
+    out = func(*args, **kwargs)
+    # print(func.__name__ + " time:", time.time() - timeA)
+    if func.__name__ in times:
+      times[func.__name__] += time.time() - timeA
+    else:
+      times[func.__name__] = time.time() - timeA
+    return out
+  return inner
 
 def main():
   containerColors = [3, 3, 3, 3] # 1 = green, 2 = blue, 3 = not scaned / error
@@ -48,10 +62,13 @@ def main():
 
   # ** MOVE BOAT TO CONTAINER PICKUP **
   straight(70)
-  durn(turn=-140, type="tank", speed=150)
-  straight(250)
-  durn(turn=80, type="tank", speed=150)
+  durn(turn=-150, type="tank", speed=150)
+  straight(150)
+  straightUntilBlack(direction=1, speed=200)
+  straight(10, speed=300)
+  durn(turn=120, type="tank", speed=150)
   sweep(sensor=RightColor, direction="left", whiteFirst=True)
+  lfpidDistance(distance=300, sensor=RightColor, sideofsensor='in', speed=400, kp=0.4)
   lfpidBlack(sensor=RightColor, sideofsensor='in', blackthreshold=10, whitethreshold=25, speed=150, kp=0.4)
   lfpidDistance(distance=110, sensor=RightColor, sideofsensor='in', speed=150, kp=0.4)
   durn(turn=120, type="tank", speed=150)
@@ -97,7 +114,7 @@ def main():
       time.sleep(0.3)
     else: # blue
       armGrab("up->down")
-      armGrab("down->midup", speed=250)
+      armGrab("down->midup", speed=150)
       newPosition, boatIndex = closestBoat(position, boatPositions, boatAvailable)
       position += straight(newPosition - position, deceleration=True)
       boatGrab(movement="close", hold=True, speed=200)
@@ -125,22 +142,21 @@ def main():
   _thread.start_new_thread(boatGrab, ("open", 1.3))
 
   # ** SMALL BOAT CONTAINER PICKUP **
-  print(containerColors)
   newPosition, containerIndex = closestContainer(position, containerPositions, containerColors, markingBlocks, useMarkingBlocks=False)
   position += straight(newPosition - position, deceleration=True)
-  print(containerColors)
   armGrab("up->down")
   armGrab("down->midup", speed=(200 if containerColors[containerIndex] == 1 else 400))
-  print(containerColors)
   containerColors[containerIndex] = 3
-  print(containerColors)
   newPosition, containerIndex = closestContainer(position, containerPositions, containerColors, markingBlocks, useMarkingBlocks=False)
   position += straight(newPosition - position, deceleration=True)
-  print(containerColors)
   armGrab("midup->down")
   armGrab("down->mid", speed=(200 if containerColors[containerIndex] == 1 else 400))
-  straight(400)
+  straight(650)
+  durn(turn=-150, type="tank", speed=400)
+  straight(250)
+  straightUntilBlack(direction=1, speed=200)
 
+@timefunc
 def calibratePos(position):
   if position > 600:
     straightUntilBlack(direction=1, speed=100)
@@ -152,38 +168,25 @@ def calibratePos(position):
     straightUntilBlack(direction=1, speed=200)
     return -15
 
-def closestBoat(position, boatPositions, boatAvailable, exclude=[]):
-  closest = 0
-  while closest in exclude:
-    closest += 1
+@timefunc
+def closestBoat(position, boatPositions, boatAvailable):
+  distances = []
   for i, container in enumerate(boatPositions):
-    if abs(container - position) < abs(boatPositions[closest] - position) and i not in exclude:
-      closest = i
-  if boatAvailable[closest]:
-    print("boat", boatPositions[closest], closest)
-    return boatPositions[closest], closest
-  else:
-    if len(exclude) == 3:
-      return None
-    exclude.append(closest)
-    return closestBoat(position, boatPositions, boatAvailable, exclude=exclude)
+    if boatAvailable[i]:
+      distances.append((abs(container - position), i))
+  distances.sort(key=lambda x: x[0])
+  return boatPositions[distances[0][1]], distances[0][1]
 
-def closestContainer(position, containerPositions, containerColors, markingBlocks, useMarkingBlocks=True, exclude=[]):
-  closest = 0
-  while closest in exclude:
-    closest += 1
+@timefunc
+def closestContainer(position, containerPositions, containerColors, markingBlocks, useMarkingBlocks=True):
+  distances = []
   for i, container in enumerate(containerPositions):
-    if abs(container - position) < abs(containerPositions[closest] - position) and i not in exclude:
-      closest = i
-  if (containerColors[closest] in markingBlocks or not useMarkingBlocks) and containerColors[closest] != 3:
-    print("container", containerPositions[closest], closest)
-    return containerPositions[closest], closest
-  else:
-    if len(exclude) == 3:
-      return None
-    exclude.append(closest)
-    return closestContainer(position, containerPositions, containerColors, markingBlocks, exclude=exclude)
+    if containerColors[i] in markingBlocks or (not useMarkingBlocks and containerColors[i] != 3):
+      distances.append((abs(container - position), i))
+  distances.sort(key=lambda x: x[0])
+  return containerPositions[distances[0][1]], distances[0][1]
 
+@timefunc
 def calculateColors(colorList, replaceRandomly=False):
   if colorList.count(3) == 2 and (colorList.count(1) == 2 or colorList.count(2) == 2):
     colorList = list(map(lambda x: (1 if colorList.count(2) == 2 else 2) if x == 3 else x, colorList))
@@ -209,26 +212,6 @@ def calculateColors(colorList, replaceRandomly=False):
             colorList[i] = random.randint(1, 2)
     return (colorList, False)
 
-def markingBlockScan(duration): # not used, maybe remove
-  colorList = []
-  newStartTime = time.time()
-  while abs(newStartTime - time.time()) < abs(duration):
-    colorList.append(rgbtocolor(ColorA.rgb()))
-  global markingBlocks
-  markingBlocks = []
-  if colorList.count(1) / len(colorList) > 0.05:
-    markingBlocks.append(1)
-  if colorList.count(2) / len(colorList) > 0.05:
-    markingBlocks.append(2)
-  if len(markingBlocks) == 1:
-    markingBlocks = [markingBlocks[0], markingBlocks[0]]
-  print(markingBlocks)
-
-def recordrli(distance):                                                         
-  startdistance = robot.distance()
-  while abs(robot.distance() - startdistance) < abs(distance):
-    print(RightColor.reflection(), LeftColor.reflection())
-
 def rgbtocolor(rgb): # None = 0, green = 1, blue = 2
   if sum(rgb) < 4:
     return 0
@@ -239,6 +222,7 @@ def rgbtocolor(rgb): # None = 0, green = 1, blue = 2
   else:
     return 0
 
+@timefunc
 def straight(distance, speed=400, deceleration=False):
   if distance < 0:
     speed *= -1
@@ -253,6 +237,7 @@ def straight(distance, speed=400, deceleration=False):
   robot.stop()
   return distance
 
+@timefunc
 def straightUntilBlack(direction=1, speed=400):
   while (RightColor.reflection() + LeftColor.reflection()) / 2 < 35:
     robot.drive(speed * direction, 0)
@@ -260,69 +245,7 @@ def straightUntilBlack(direction=1, speed=400):
     robot.drive(speed * direction / 2, 0)
   robot.stop()
 
-'''
-def straightwithadjust(scandistance, movedistance, changefactor, scanspeed=150, movespeed=400):
-  if movedistance < 100:
-    raise Exception('movedistance must be greater than 100')
-  if scandistance < 0:
-    scanspeed *= -1
-  if movedistance < 0:
-    movespeed *= -1
-  out = []
-  startdistance = robot.distance()
-  while abs(robot.distance() - startdistance) < abs(scandistance):
-    robot.drive(scanspeed, 0)
-    out.append([RightColor.reflection(), LeftColor.reflection()])
-  robot.stop()
-  newout = [[out[0][0]], [out[0][1]]]
-  for i in range(1, len(out)):
-    newout[0].append(out[i][0])
-    newout[1].append(out[i][1])
-  print(newout)
-  shift = getdriveoverangle(newout)
-  print(shift)
-  quit()
-  startdistance = robot.distance()
-  while abs(robot.distance() - startdistance) < 100:
-    robot.drive(movespeed, shift * changefactor)
-  robot.stop()
-  while abs(robot.distance() - startdistance) < abs(movedistance) - 100:
-    robot.drive(movespeed, 0)
-  robot.stop()
-'''
-
-def square(threshold, speed):
-  leftBlack = False
-  rigthtBlack = False
-  if LeftColor.reflection() <= threshold:
-      leftBlack = True
-  if RightColor.reflection() <= threshold:
-    rigthtBlack = True
-  if not leftBlack and not rigthtBlack:
-    RightMotor.run(speed)
-    LeftMotor.run(speed)
-  while not leftBlack and not rigthtBlack:
-    if LeftColor.reflection() <= threshold:
-      leftBlack = True
-      LeftMotor.hold()
-    elif RightColor.reflection() <= threshold:
-      rigthtBlack = True
-      RightMotor.hold()
-  
-  if not rigthtBlack:
-    RightMotor.run(speed)
-  while not rigthtBlack:
-    if RightColor.reflection() <= threshold:
-      rigthtBlack = True
-      RightMotor.hold()
-  
-  if not leftBlack:
-    LeftMotor.run(speed)
-  while not leftBlack:
-    if LeftColor.reflection() <= threshold:
-      leftBlack = True
-      LeftMotor.hold()
-
+@timefunc
 def durn(turn, circleradius=30, type='tank', fb='forward', speed=200): # durn = degree turn
   if type not in ['tank', 'pivot', 'circle']:
     raise Exception('type must be "tank" or "pivot" or "circle"')
@@ -358,6 +281,7 @@ def durn(turn, circleradius=30, type='tank', fb='forward', speed=200): # durn = 
     LeftMotor.hold()
     RightMotor.hold()
 
+@timefunc
 def lfpidBlack(sensor=RightColor, sideofsensor='in', blacks=1, waitdistance=25, startdistance=0, kp=0.25, ki=0, kd=0.5, speed=[], estdistance=0, blackthreshold=10, whitethreshold=None): # wait distance is the # of mm after a black it waits until continue detecting blacks
   if sensor not in [RightColor, LeftColor]:
     raise Exception('sensor must be RightColor or LeftColor')
@@ -417,6 +341,7 @@ def lfpidBlack(sensor=RightColor, sideofsensor='in', blacks=1, waitdistance=25, 
 
   robot.stop()
 
+@timefunc
 def lfpidDistance(distance, sensor=RightColor, sideofsensor='in', speed=[], kp=0.25, ki=0, kd=0.5):
   if sensor not in [RightColor, LeftColor]:
     raise Exception('sensor must be RightColor or LeftColor')
@@ -464,6 +389,7 @@ def lfpidDistance(distance, sensor=RightColor, sideofsensor='in', speed=[], kp=0
 
   robot.stop()
 
+@timefunc
 def boatGrab(movement="open", percentage=1, hold=False, speed=400):
   if movement == "open":
     BoatMotor.stop()
@@ -475,6 +401,7 @@ def boatGrab(movement="open", percentage=1, hold=False, speed=400):
     if hold:
       BoatMotor.hold()
 
+@timefunc
 def armGrab(movement, speed=None):
   if movement not in ['up->down', 'down->up', 'down->mid', 'mid->up', 'up->mid', 'mid->down', 'down->midup', 'midup->up', 'midup->down']:
     raise Exception('movement must be "up->down", "down->up", "down->mid", "mid->up", "up->mid", or "mid->down"')
@@ -517,6 +444,7 @@ def armGrab(movement, speed=None):
     time.sleep(0.15)
     ArmMotor.hold()
 
+@timefunc
 def sweep(sensor, direction, speed=100, whiteFirst=False):
   if sensor not in [RightColor, LeftColor]:
     raise Exception('sensor must be RightColor or LeftColor')
@@ -562,6 +490,7 @@ def sweep(sensor, direction, speed=100, whiteFirst=False):
 
   durn(robot.angle() - targetangle, type='tank', fb='forward', speed=speed)
 
+@timefunc
 def sTurn(rl, fb, turn, type='pivot', drive=0, turnSpeed=100): # rl = right-left, fb = forward-backward, turn = turn degrees(posotive), drive = drive between turns(positive)
   if rl not in ['right', 'left']:
     raise Exception('rl must be "right" or "left"')
@@ -583,114 +512,7 @@ def sTurn(rl, fb, turn, type='pivot', drive=0, turnSpeed=100): # rl = right-left
     straight(drive)
   durn(conificient * (startangle - robot.angle()), type=type, fb=fb, speed=turnSpeed)
 
-def fixdata(data, window_size=21, order=5):
-  a = SavitzkyGolayFilter(data, window_size=window_size, order=order)
-
-  for j in range(len(a) - 1):
-    if a[j + 1] - a[j] > 4: 	
-      f = [a[j]] * len(a[:j])
-      g = a[j:]
-      f.extend(g)
-      a = f.copy()
-      break
-
-  for j in reversed(range(len(a) - 1)):
-    if a[j] - a[j + 1] > 4:
-      f = a[:j + 1]
-      g = [a[j + 1]] * len(a[j + 1:])
-      f.extend(g)
-      a = f.copy()
-      break
-
-  fixeddata = []
-  for j in range(len(a)):
-    fixeddata.append(a[j] / max(a))
-  localMaxOut = localMaxList(fixeddata)
-  localMinOut = localMinList(fixeddata)
-
-  if len(localMaxOut) != 2 or len(localMinOut) != 1:
-    return None
-  return [localMaxOut[0], localMaxOut[1], localMinOut[0]]
-
-def SavitzkyGolayFilter(data, window_size=21, order=5):
-  half_window = (window_size - 1) // 2
-  firstvals = [-abs(i - data[0]) + data[0] for i in data[1:half_window+1][::-1]]
-  lastvals = [abs(i - data[-1]) + data[-1] for i in data[-half_window-1:-1][::-1]]
-  data = firstvals + data + lastvals
-
-  return convolve(data, SavitzkyGolayFilterData[str(window_size) + "," + str(order)])
-  
-def convolve(data, f):
-  out = []
-  for i in range(len(data) - len(f) + 1):
-    out.append(sum([data[i + j] * f[j] for j in range(len(f))]))
-  return out
-
-def localMaxList(data):
-  order=1
-
-  locs = list(range(0, len(data)))                                                                                                                                                                                                                                                                                                                                                                           
-
-  results = [True] * len(data)
-  main = take(data, locs)
-  for shift in range(1, order + 1):
-    plus = take(data, [i + shift for i in locs])
-    minus = take(data, [i - shift for i in locs])
-    results = andequal(results, [main[i] > plus[i] for i in range(len(main))])
-    results = andequal(results, [main[i] > minus[i] for i in range(len(main))])
-  return [i for i, val in enumerate(results) if val]
-
-def localMinList(data):
-  order=1
-
-  locs = list(range(0, len(data)))
-
-  results = [True] * len(data)
-  main = take(data, locs)
-  for shift in range(1, order + 1):
-    plus = take(data, [i + shift for i in locs])
-    minus = take(data, [i - shift for i in locs])
-    results = andequal(results, [main[i] < plus[i] for i in range(len(main))])
-    results = andequal(results, [main[i] < minus[i] for i in range(len(main))])
-  return [i for i, val in enumerate(results) if val]
-
-def take(array, indices):
-  out = []
-  for i in indices:
-    if i < len(array):
-      out.append(array[i])
-    else:
-      out.append(array[len(array) - 1])
-  return out
-
-def andequal(list1, list2):
-  out = []
-  for i in range(len(list1)):
-    if list1[i] == list2[i]:
-      out.append(list1[i])
-    else:
-      out.append(False)
-  return out
-
-def getdriveoverangle(data):
-  allminmax = []
-  for i in range(len(data)):
-    breakOn = False
-    for j in range(7, 15, 2):
-      if breakOn:
-        break
-      for k in range(21, 31, 2):
-        if breakOn:
-          break
-        print(i, j, k)
-        a = fixdata(data[i], k, j)
-        if a != None:
-          breakOn = True
-          allminmax.append(a)
-          break
-  out = [allminmax[0][i] - allminmax[1][i] for i in range(len(allminmax[0]))]
-  return sum(out) / len(out)
-
+@timefunc
 def colorScan(acceptable, direction, errorNum, outTurnIncrease=1, speed=200):
   if rgbtocolor(ColorA.rgb()) in acceptable:
     return rgbtocolor(ColorA.rgb())
@@ -745,4 +567,6 @@ def mode(List):
 starttime = time.time()
 main()
 robot.stop()
-print(time.time() - starttime)
+for i in times:
+  print("total " + i + " time:", times[i])
+print("\ntotal time:", time.time() - starttime)
